aa5fe00e13775af3e4333c11d6dda9e7aa96c019
Danail Videv <danailvidev@gmail.com>
2018-09-08 12:51:35 +0300

    load courses, courses list

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,39 +2,15 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
-import {PropTypes} from 'prop-types';
+import CourseList from './CourseList';
 
 class CoursesPage extends Component {
-  static initialState = () => ({
-    course: {
-      title: ''
-    }
-  });
-  state = CoursesPage.initialState();
-
-  onTitleChange = (event) => {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({course: course});
-  };
-
-  onClickSave = () => {
-    this.props.actions.createCourse(this.state.course);
-    this.setState(CoursesPage.initialState());
-  };
-
-  courseRow(course, index) {
-    return <div key={index}>{course.title}</div>
-  }
-
   render() {
+    const {courses} = this.props;
     return (
       <div>
         <h1>Courses</h1>
-        {this.props.courses.map(this.courseRow)}
-        <h2>Add course</h2>
-        <input type="text" onChange={this.onTitleChange} value={this.state.course.title}/>
-        <input type="submit" value="Save" onClick={this.onClickSave}/>
+        <CourseList courses={courses}/>
       </div>
     );
   }
@@ -54,7 +30,3 @@ function mapDispatchToProps(dispatch) {
 
 export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
 
-CoursesPage.propTypes = {
-  courses: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
-};
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
+import { loadCourses} from "./actions/courseActions";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from "./components/App";
 
 const store = configureStore();
+store.dispatch(loadCourses());
 
 ReactDOM.render(
   <Provider store={store}><App/></Provider>,
